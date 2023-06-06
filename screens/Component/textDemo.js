66c2da8793d1deff84a1ede98e782b0216e36a2a
Shivansh002 <135096832+Shivansh002@users.noreply.github.com>
2023-06-06 11:13:01 +0530

    s

--- screens/Component/textDemo.js
+++ screens/Component/textDemo.js
@@ -1,24 +1,24 @@
-import React, {useState} from 'react';
-import {ScrollView, Text, TextInput, View} from 'react-native';
+import React, { useState } from "react";
+import { ScrollView, Text, TextInput, View } from "react-native";
 
-import textStyle from './textStyle.style';
+import textStyle from "./textStyle.style";
 
 function TextDemo() {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
 
   return (
     <View>
       <TextInput
         style={textStyle.inputField}
-        onChangeText={newText => setText(newText)}
+        onChangeText={(newText) => setText(newText)}
         placeholder="Write a sentence"
         multiline={true}
         maxLength={60}
         blurOnSubmit={true}
         numberOfLines={2}
-        cursorColor={'purple'}
+        cursorColor={"purple"}
         editable={true}
-        enterKeyHint={'done'}
+        enterKeyHint={"done"}
         importantForAutofill="yes"
         inlineImageLeft="random"
         inputMode="text"
@@ -27,24 +27,28 @@ function TextDemo() {
 
       <Text
         numberOfLines={1}
-        style={[textStyle.normalText, textStyle.textContainer]}>
+        style={[textStyle.normalText, textStyle.textContainer]}
+      >
         {text}
       </Text>
 
       <Text
         numberOfLines={1}
-        style={[textStyle.boldText, textStyle.textContainer]}>
+        style={[textStyle.boldText, textStyle.textContainer]}
+      >
         {text}
       </Text>
       <Text
         numberOfLines={1}
         style={[textStyle.disabledText, textStyle.textContainer]}
-        disabled>
+        disabled
+      >
         {text}
       </Text>
       <Text
         numberOfLines={1}
-        style={[textStyle.italicText, textStyle.textContainer]}>
+        style={[textStyle.italicText, textStyle.textContainer]}
+      >
         {text}
       </Text>
     </View>
